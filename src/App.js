e8a18279942924b98dc3b1a1a227b6a1a503ce5f
Lorene Pecci <lorenepecci@gmail.com>
2022-02-09 18:52:00 -0300

    req5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home';
-import Carrinho from './Pages/Carrinho'
+import Carrinho from './pages/Carrinho';
+import Home from './pages/Home';
 
 function App() {
   return (
