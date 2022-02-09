5d0596b30fdf7297c38f62c68b618833aadf1e0e
Geovanna <geovanna.carolline.gcs@gmail.com>
2022-02-09 16:25:36 -0400

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import Carrinho from './Pages/Carrinho'
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={ Home } />
+      <Route path="/carrinho" component={ Carrinho } />
     </BrowserRouter>
 
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 /* import {BrowserRouter} from 'react-router-dom' */
 
 class Home extends React.Component {
@@ -15,6 +16,14 @@ class Home extends React.Component {
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        <div>
+          <Link 
+            to={"/carrinho"}
+            data-testid="shopping-cart-button"
+          >
+            Carrinho
+          </Link>
+        </div>
       </>
     );
   }
