c4102950e48941ddb3d4b0f511114a5ebc4eec3f
Lorene Pecci <lorenepecci@gmail.com>
2022-02-10 17:09:06 -0300

    req7 finalizado

--- src/pages/DetalheProduto.js
+++ src/pages/DetalheProduto.js
@@ -1,8 +1,17 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { getProductsFromCategoryAndQuery } from '../services/api';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getProductsID } from '../services/api';
 
 class DetalheProduto extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dadosDoProduto: {},
+      arrayAttributes: [],
+    };
+  }
+
   componentDidMount() {
     const { match: { params: { id } } } = this.props;
     console.log(id);
@@ -10,16 +19,53 @@ class DetalheProduto extends React.Component {
   }
 
   onSubmitSearch = async (id) => {
-    const dadosProduto = await getProductsFromCategoryAndQuery('', id);
+    const dadosProduto = await getProductsID(id);
     console.log(dadosProduto);
-    // this.setState({
-    //   listaProdutos: listaProduto.results,
-    // });
+    this.setState({
+      dadosDoProduto: dadosProduto,
+      arrayAttributes: dadosProduto.attributes,
+    });
+    const { arrayAttributes } = this.state;
+    console.log(arrayAttributes);
   }
 
   render() {
+    const { dadosDoProduto, arrayAttributes } = this.state;
     return (
-      <div />
+      <div>
+        <h1>Especificações Técnicas</h1>
+        <Link
+          to="/carrinho"
+          data-testid="shopping-cart-button"
+        >
+          Carrinho
+        </Link>
+        <h3 data-testid="product-detail-name">{ dadosDoProduto.title }</h3>
+        <img
+          src={ dadosDoProduto.thumbnail }
+          width="300"
+          alt={ dadosDoProduto.id }
+        />
+        <h2>
+          R$:
+          { dadosDoProduto.price }
+        </h2>
+        <div className="especificacoes">
+          { arrayAttributes.map((atribute, index) => (
+            <div key={ index }>
+              <span>
+
+                {' '}
+                { atribute.name }
+                :
+                { '  '}
+              </span>
+              <span>ausihsauisah</span>
+            </div>
+          ))}
+        </div>
+
+      </div>
     );
   }
 }
