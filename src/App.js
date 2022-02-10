36e47d9444445d6592248618e953638ef940e297
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-02-10 16:05:05 -0300

    Criado a class Detalhe Produto

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Carrinho from './pages/Carrinho';
+import DetalheProduto from './pages/DetalheProduto';
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={ Home } />
       <Route path="/carrinho" component={ Carrinho } />
+      <Route path="/detalhe/:id" render={ (props) => <DetalheProduto { ...props } /> } />
     </BrowserRouter>
-
   );
 }
 
--- src/pages/DetalheProduto.js
+++ src/pages/DetalheProduto.js
@@ -1,25 +1,35 @@
-import React from "react";
-import PropTypes from 'prop-types'; 
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class DetalheProduto extends React.Component {
   componentDidMount() {
     const { match: { params: { id } } } = this.props;
     console.log(id);
+    this.onSubmitSearch(id);
+  }
+
+  onSubmitSearch = async (id) => {
+    const dadosProduto = await getProductsFromCategoryAndQuery({ id });
+    console.log(dadosProduto);
+    // this.setState({
+    //   listaProdutos: listaProduto.results,
+    // });
   }
 
   render() {
     return (
-
-    )
+      <div />
+    );
   }
 }
 
 DetalheProduto.propTypes = {
   match: PropTypes.shape({
-      params: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-      }),
-    }).isRequired,
-  }; 
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default DetalheProduto;
