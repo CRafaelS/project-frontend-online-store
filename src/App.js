a49039ceabf71f927503cee09acde4979a07d78f
Geovanna <geovanna.carolline.gcs@gmail.com>
2022-02-10 16:32:39 -0400

    Configura LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
 import Carrinho from './pages/Carrinho';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Route path="/" component={ Home } />
-      <Route path="/carrinho" component={ Carrinho } />
-    </BrowserRouter>
+class App extends Component {
+  constructor() {
+    super();
+    if (!localStorage.getItem('cartTrybe')) {
+      localStorage.setItem('cartTrybe', JSON.stringify([]));
+    }
+  }
 
-  );
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ Home } />
+        <Route path="/carrinho" component={ Carrinho } />
+      </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,6 +2,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 class ProductCard extends React.Component {
+  handleClick = (event) => {
+    const { id } = event.target;
+    const cart = JSON.parse(localStorage.getItem('cartTrybe'));
+    if (cart.some((product) => product.id === id)) {
+      cart.forEach((product, index) => {
+        if (product.id === id) {
+          cart[index].quantity += 1;
+        }
+      });
+    } else {
+      cart.push({
+        id,
+        quantity: 1,
+      });
+    }
+    localStorage.setItem('cartTrybe', JSON.stringify(cart));
+  }
+
   render() {
     const { listaProdutos } = this.props;
     return (
@@ -18,7 +36,15 @@ class ProductCard extends React.Component {
               width="150"
               height="200"
             />
-            <h3>{produto.price}</h3>
+            <h3>{ produto.price }</h3>
+            <button
+              id={ produto.id }
+              type="button"
+              onClick={ this.handleClick }
+              data-testid="product-add-to-cart"
+            >
+              Adicionar  ao carrinho
+            </button>
           </div>
         ))}
       </div>
